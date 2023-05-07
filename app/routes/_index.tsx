269ea88831a5f3c8b07d59a8b3d28e1f207b26a5
satishnvrn <nvrn.satish@gmail.com>
2023-05-08 00:21:38 +0530

    Initial commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,20 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "Remix Tailwind Boilerplate" }];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1 className="text-3xl font-bold">Welcome to Remix and Tailwind Boilerplate</h1>
+      <ul>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
